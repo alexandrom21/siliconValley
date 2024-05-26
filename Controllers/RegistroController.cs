ddb0c0fa06ed308265fb6ac32968c504a5ef4685
alexandrom21 <alexandro_medina@usmp.pe>
2024-05-26 09:20:51 -0500

    Cambios en Registro

--- Controllers/RegistroController.cs
+++ Controllers/RegistroController.cs
@@ -1,31 +1,157 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using siliconValley.Data;
+using siliconValley.Models;
 
 namespace siliconValley.Controllers
 {
     public class RegistroController : Controller
     {
-        private readonly ILogger<RegistroController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public RegistroController(ILogger<RegistroController> logger)
+        public RegistroController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Registro
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.DataRegistro.ToListAsync());
+        }
+
+        // GET: Registro/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registro = await _context.DataRegistro
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (registro == null)
+            {
+                return NotFound();
+            }
+
+            return View(registro);
+        }
+
+        // GET: Registro/Create
+        public IActionResult Create()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // POST: Registro/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Job")] Registro registro)
         {
-            return View("Error!");
+            if (ModelState.IsValid)
+            {
+                _context.Add(registro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(registro);
+        }
+
+        // GET: Registro/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registro = await _context.DataRegistro.FindAsync(id);
+            if (registro == null)
+            {
+                return NotFound();
+            }
+            return View(registro);
+        }
+
+        // POST: Registro/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Job")] Registro registro)
+        {
+            if (id != registro.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(registro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RegistroExists(registro.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(registro);
+        }
+
+        // GET: Registro/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registro = await _context.DataRegistro
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (registro == null)
+            {
+                return NotFound();
+            }
+
+            return View(registro);
+        }
+
+        // POST: Registro/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var registro = await _context.DataRegistro.FindAsync(id);
+            if (registro != null)
+            {
+                _context.DataRegistro.Remove(registro);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RegistroExists(int id)
+        {
+            return _context.DataRegistro.Any(e => e.Id == id);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using siliconValley.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,16 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API",
+        Version = "v1",
+        Description = "Descripción de la API"
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,6 +44,13 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
